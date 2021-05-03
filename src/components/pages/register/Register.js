a08969f89a8a9e6e4b9621f81c44ab389653fbe0
Marcixy <boersenanalysten@gmail.com>
2021-05-03 20:23:26 +0200

    Registrieren Seite angefangen
    - Registrieren Seite angefangen zu implementieren.

--- src/components/pages/register/Register.js
+++ src/components/pages/register/Register.js
@@ -1,9 +1,27 @@
+// material-ui imports
+import {
+    Button,
+    TextField
+} from '@material-ui/core';
+
+// third-party imports
+import { Link } from 'react-router-dom';
+
 import './Register.css';
 
 function Register() {
     return (
         <div className="register-page">
-            <h2>Registrieren</h2>
+            <div className="register-form">
+                <h2>Registrieren</h2>
+                <TextField label="E-Mail" />
+                <TextField label="Benutzername" />
+                <TextField label="Passwort" />
+                <Button variant="contained" color="primary">Registrieren</Button>
+                <Link to="/login">
+                    Zum Login
+                </Link>
+            </div>
         </div>
     )
 }
