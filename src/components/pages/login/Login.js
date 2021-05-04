d45ab8c18b9b033f0257755fa375bd50fc51564e
Marcixy <boersenanalysten@gmail.com>
2021-05-04 18:28:17 +0200

    Login Seite angefangen
    - Login Seite angefangen zu implementieren.

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,9 +1,26 @@
+// material-ui imports
+import {
+    Button,
+    TextField
+} from '@material-ui/core';
+
+// third-party imports
+import { Link } from 'react-router-dom';
+
 import './Login.css';
 
 function Login() {
     return (
         <div className="login-page">
-            <h2>Login</h2>
+            <div className="login-form">
+                <h2>Login</h2>
+                <TextField label="E-Mail" />
+                <TextField label="Passwort" />
+                <Button variant="contained" color="primary">Login</Button>
+                <Link to="/register">
+                    Zur Registrierung
+                </Link>
+            </div>
         </div>
     )
 }
