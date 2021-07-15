427b1a3d30c62d5d7b14aa5fcdec1f826a0fb38b
Marcixy <boersenanalysten@gmail.com>
2021-07-15 20:27:35 +0200

    Hilfe Seite angefangen zu schreiben
    - Hilfe Seite angefangen zu schreiben.

--- src/client/components/pages/help/Help.js
+++ src/client/components/pages/help/Help.js
@@ -3,7 +3,20 @@ import './Help.css';
 function Help() {
     return (
         <div className="help-page">
-            <h2>Hilfe</h2>
+            <h2>Willkommen bei Börsenanalysten</h2>
+            <p>Börsenanalysten ist eine Plattform für Menschen die sich für die Themen Börse, Aktien, ETFs, Vermögensaufbau und viele weitere Finanzthemen interessieren.
+                Auf der Plattform könnt Ihr eure Fragen stellen, Antworten geben und allgemeine Beiträge zu Finanzthemen erstellen.</p>
+            <p id="help-page-header-text">Börsenanalysten ist dabei etwas anders als andere Seiten. Hier ist warum:</p>
+
+            <h3>Fragen & Antworten, keine Ablenkungen</h3>
+
+            <h3>Up- und Downvoting von Beiträgen & Antworten</h3>
+
+            <h3>Tags hinzufügen zum strukturieren</h3>
+
+            <h3>Alle Beiträge filtern und die besten Beiträge finden</h3>
+
+            <h3>Aktienanteile sammeln für hilfreiche Beiträge und Antworten</h3>
         </div>
     )
 }
